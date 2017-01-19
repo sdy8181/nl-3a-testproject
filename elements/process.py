6f41948ab55ca3f8287c8ba09ebc446a260478f9
ouguangqian <guangqianou@pateo.com.cn>
2017-01-19 14:01:38 +0800

    关闭指定进程失败

--- elements/process.py
+++ elements/process.py
@@ -34,6 +34,20 @@ class Process:
         """
         return d(resourceId=self.__pkg_name + ':id/kill_process_layout')
 
+    def get_process_name_eles(self):
+        """
+        获取进程名称控件
+        :return:
+        """
+        return d(resourceId=self.__pkg_name + ':id/process_name')
+
+    def get_process_kill_it_eles(self):
+        """
+        获取进程上面的关闭控件
+        :return:
+        """
+        return d(resourceId=self.__pkg_name + ':id/kill_it')
+
     def get_kill_process_btn_by_name(self, process_name):
         """
         根据名字获取关闭进程控件
@@ -42,11 +56,17 @@ class Process:
         """
         process_view = self.get_processes_view()
         if process_view.scroll.vert.to(text=process_name):
-            process_layout = self.get_process_eles()
-            for p in process_layout:
-                if p.child_by_text(process_name).exists:
-                    kill_it = p.child(resourceId=self.__pkg_name + ':id/kill_it')
-                    return kill_it
+            process_name_ele_list = self.get_process_name_eles()
+            kill_it_ele_list = self.get_process_kill_it_eles()
+            for i in range(len(process_name_ele_list)):
+                if process_name_ele_list[i].text.strip() == process_name:
+                    return kill_it_ele_list[i]
+
+            # process_layout = self.get_process_eles()
+            # for p in process_layout:
+            #     if p.child_by_text(process_name).exists:
+            #         kill_it = p.child(resourceId=self.__pkg_name + ':id/kill_it')
+            #         return kill_it
             else:
                 uit.raise_Exception_info('没有找到指定进程关闭控件')
         else:
