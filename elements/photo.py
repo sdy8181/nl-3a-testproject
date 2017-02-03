7f3f33aa615b3c679d716573d481a7bb70ae607c
ouguangqian <guangqianou@pateo.com.cn>
2017-02-03 18:03:56 +0800

    更新图片应用步骤实现

---
+++ elements/photo.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2/3/17
+@author: 欧光乾
+@email: guangqianou@pateo.com.cn
+"""
+from utils.helpTools import d
+from utils.uiTools import uit
+
+
+class Photo:
+    """
+    图片应用的操作
+    """
+    def __init__(self):
+        """
+        初始化图片的包名
+        """
+        self.__pkg_name = 'com.pateo.as.photo'
+
+    def get_photo_folder_gridview(self):
+        """
+        获取图片文件夹列表视图
+        :return:
+        """
+        return d(resourceId=self.__pkg_name + ':id/gridview')
+
+    def get_photo_folder_by_name(self, folder_name):
+        """
+        根据文件夹名称获取图片文件夹
+        :param folder_name:
+        :return:
+        """
+
+        folder_search_txt = folder_name + ' ('
+        # 获取文件夹列表视图控件
+        grid_view_ele = self.get_photo_folder_gridview()
+        if not grid_view_ele.wait.exists():
+            uit.raise_Exception_info('图片列表视图控件不存在')
+        else:
+            # 滑动到指定文件夹 没有则抛出异常
+            folder_exists_flag = grid_view_ele.scroll.vert.to(
+                    textContains=folder_search_txt)
+            if folder_exists_flag:
+                return d(textContains=folder_search_txt)
+            else:
+                uit.raise_Exception_info('指定图片文件夹不存在')
+
+    def get_photo_gridview(self):
+        """
+        获取图片列表视图
+        :return:
+        """
+        return d(resourceId=self.__pkg_name + ':id/id_coverflow')
+
+    def get_photo_by_name(self, photo_name):
+        """
+        根据图片名称查找图片并返回图片对象, 查找失败抛出异常
+        :param photo_name:
+        :return:
+        """
+        photo_listview_ele = self.get_photo_gridview()
+        if not photo_listview_ele.wait.exists():
+            uit.raise_Exception_info('图片列表视图不存在')
+        else:
+            photo_flag = photo_listview_ele.scroll.vert.to(text=photo_name)
+            if photo_flag:
+                return d(text=photo_name)
+            else:
+                uit.raise_Exception_info('没有找到指定图片文件')
+
+    def get_photo_preview_prev(self):
+        """
+        获取图片预览界面的上一张图片控件
+        :return:
+        """
+        return d(resourceId=self.__pkg_name + ':id/id_toolbar_btn_prev')
+
+    def get_photo_preview_next(self):
+        """
+        获取图片预览界面的下一张图片控件
+        :return:
+        """
+        return d(resourceId=self.__pkg_name + ':id/id_toolbar_btn_next')
+
+    def get_photo_preview_del(self):
+        """
+        获取图片删除按钮
+        :return:
+        """
+        return d(resourceId=self.__pkg_name + ':id/id_toolbar_btn_delete')
+
+    def get_photo_preview_fullscreen(self):
+        """
+        获取图片预览界面的全屏控件
+        :return:
+        """
+        return d(resourceId=self.__pkg_name + ':id/id_toolbar_btn_fullscreen')
+
+    def get_photo_preview_name(self):
+        """
+        获取图片预览界面的图片名称
+        :return:
+        """
+        return d(resourceId=self.__pkg_name + ':id/id_photo_name')
+
+
+
+photo = Photo()
+
+
+
+
+
---
+++ steps/step_photo.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2/3/17
+@author: 欧光乾
+@email: guangqianou@pateo.com.cn
+"""
+import time
+from behave import when
+from behave import then
+
+from utils.helpTools import d, MAP_VAR
+from elements.photo import photo
+from utils.uiTools import uit
+
+
+@when(u'< 点击图片文件夹')
+def step_impl(context):
+    """
+    点击图片文件夹 文件夹名称不包含图片数量
+    :param context:
+    :return:
+    """
+    # 获取文件夹名称
+    folder_name = context.table[0]['folder_name']
+    folder_ele = photo.get_photo_folder_by_name(folder_name)
+
+    if folder_ele.wait.exists():
+        pos_x, pos_y = uit.get_clickcoord_by_ele(folder_ele)
+        d.click(pos_x, pos_y)
+    else:
+        uit.raise_Exception_info('指定文件夹不存在')
+
+
+@when(u'< 点击图片文件')
+def step_impl(context):
+    """
+    点击图片文件， 图片文件带有后缀
+    :param context:
+    :return:
+    """
+    # 获取图片文件名参数
+    photo_name = context.table[0]['photo_name']
+    photo_ele = photo.get_photo_by_name(photo_name)
+
+    if photo_ele.wait.exists():
+        pos_x, pos_y = uit.get_clickcoord_by_ele(photo_ele)
+        d.click(pos_x, pos_y)
+    else:
+        uit.raise_Exception_info('指定图片文件不存在')
+
+
+@when(u'< 点击下一张图片')
+def step_impl(context):
+    """
+    点击下一张图片
+    :param context:
+    :return:
+    """
+    next_ele = photo.get_photo_preview_next()
+    if next_ele.wait.exists():
+        next_ele.click()
+    else:
+        uit.raise_Exception_info('下一张图片按钮不存在')
+
+
+@when(u'< 点击上一张图片')
+def step_impl(context):
+    """
+    点击上一张图片
+    :param context:
+    :return:
+    """
+    prev_ele = photo.get_photo_preview_prev()
+    if prev_ele.wait.exists():
+        prev_ele.click()
+    else:
+        uit.raise_Exception_info('上一张图片按钮不存在')
+
+
+@when(u'< 点击图片删除按钮')
+def step_impl(context):
+    """
+    点击图片删除按钮
+    :param context:
+    :return:
+    """
+    # 获取是否删除选项
+    choice = context.table[0]['choice'].strip()
+    del_ele = photo.get_photo_preview_del()
+    if del_ele.wait.exists():
+        del_ele.click()
+        # 等待2S
+        time.sleep(2)
+        choice_ele = d(text=choice)
+        if choice_ele.wait.exists():
+            choice_ele.click()
+        else:
+            uit.raise_Exception_info('是否删除选项没有找到')
+
+
+@when(u'< 点击图片全屏按钮')
+def step_impl(context):
+    """
+    点击底部的图片全屏按钮
+    :param context:
+    :return:
+    """
+    full_ele = photo.get_photo_preview_fullscreen()
+    if full_ele.wait.exists():
+        full_ele.click()
+    else:
+        uit.raise_Exception_info('图片全屏按钮不存在')
+
+
+@when(u'< 获取当前图片名称')
+def step_impl(context):
+    """
+    获取当前内文件并返回
+    :param context:
+    :return:
+    """
+    photo_name = context.table[0]['o_result']
+    # 获取图片名称控件
+    photo_name_ele = photo.get_photo_preview_name()
+
+    if photo_name_ele.wait.exists():
+        MAP_VAR[photo_name] = photo_name_ele.text.strip()
+    else:
+        uit.raise_Exception_info('图片名称控件不存在')
+
